8ac8a40c02893c4e34ca4a461ae1c409140e2556
Sophias Boko <ssmodoukpe@gmail.com>
2025-05-15 15:22:19 +0200

    nouvel animal

---
+++ src/l2ccowsay/new_file.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 15 15:13:55 2025
+
+@author: Modoukpè
+"""
+
+import sys
+sys.path.insert(0,r'C:\Users\Modoukpè\l2ccowsay\\')
+from characters import KITTY
+
+def mooing(txt: str = 'S2QT coding club is great'):
+    tt = f'{"="*(len(txt)+4)}\n'
+    tt += f"| {txt} |\n"
+    tt += f'{"="*(len(txt)+4)}'
+    return tt + KITTY
+print(mooing())
